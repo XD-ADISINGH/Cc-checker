d330de8c6e067853f02bdd1bd14861bcc6692386
Rohithaditya <86031501+Rohith-sreedharan@users.noreply.github.com>
2021-12-02 10:42:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ import logging
 import os
 import sys
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-#from telegram.ext.dispatcher import run_async
+from telegram.ext.dispatcher import run_async
 import re
 from luhn import *
 import pymongo
-from telegram.ext.Dispatcher import run_async
+
 
 
 '''
